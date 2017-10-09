3f2bdee269db91b7859a2d4202da95a5e3a54c54
Steven Eisinger <steven.a.eisinger@gmail.com>
2017-10-08 21:19:58 -0400

    Fixed segfault

--- src/tools.cpp
+++ src/tools.cpp
@@ -12,38 +12,39 @@ Tools::~Tools() {}
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
   /**
+    * TODO: Fix this. Causes Segmentation fault
     * Calculate the RMSE here.
   */
   VectorXd rmse(4);
 	rmse << 0,0,0,0;
 
-	// check the validity of the following inputs:
-	//  * the estimation vector size should not be zero
-	//  * the estimation vector size should equal ground truth vector size
-    if (!estimations.size() || (ground_truth.size() != estimations.size())) {
-        cout << "you are using an empty matrix" << endl;
-        return rmse;
-    }
-    
-    vector<VectorXd> rmse_temp(4);
-    
-	//accumulate squared residuals
-	for(int i=0; i < estimations.size(); ++i) {
-		rmse_temp[i] = estimations[i] - ground_truth[i];
-		rmse_temp[i] = rmse_temp[i].array().pow(2);
-	}
+  // check the validity of the following inputs:
+  //  * the estimation vector size should not be zero
+  //  * the estimation vector size should equal ground truth vector size
+  if (!estimations.size() || (ground_truth.size() != estimations.size())) {
+    cout << "CalculateRMSE () - Error - Empty Matrix" << endl;
+    return rmse;
+  }
 
-	//calculate the mean
-	cout << rmse_temp[0] << endl;
-	for (int i=0; i < rmse_temp.size(); ++i) {
-	    rmse(i) = rmse_temp[i].sum()/rmse_temp.size();
-	}
-	
-	//calculate the squared root
-	rmse = rmse.array().sqrt();
-	
-	//return the result
-	return rmse;
+//accumulate squared residuals
+for(unsigned int i=0; i < estimations.size(); ++i){
+  
+      VectorXd residual = estimations[i] - ground_truth[i];
+  
+      //coefficient-wise multiplication
+      residual = residual.array()*residual.array();
+      rmse += residual;
+    }
+  
+    //calculate the mean
+    rmse = rmse/estimations.size();
+  
+    //calculate the squared root
+    rmse = rmse.array().sqrt();
+  
+
+  //return the result
+  return rmse;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
@@ -64,8 +65,8 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
 
   //check division by zero
   if(fabs(c1) < 0.0001){
-      cout << "CalculateJacobian () - Error - Division by Zero" << endl;
-      return Hj;
+    cout << "CalculateJacobian () - Error - Division by Zero" << endl;
+    return Hj;
   }
 
   //compute the Jacobian matrix
